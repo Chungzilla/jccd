bac89db6e0b4326abc8ec2cdd51a840e4e643f79
Chungzilla <chung.jazmine@gmail.com>
2018-09-25 21:23:43 -0400

    added ajax destroy to designs model

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,64 @@
 #     userable_type: 'Admin',
 #     userable_id: 1
 # )
+
+# # Create Colorways
+# Colorway.create(
+#     name: 'Black',
+#     description: 'Just black'
+# )
+
+# Colorway.create(
+#     name: 'Blue - Indigo',
+#     description: 'Blue with a greenish tint'
+# )
+
+# Colorway.create(
+#     name: 'Grey - Heather',
+#     description: 'Lighter grey'
+# )
+
+# Colorway.create(
+#     name: 'Green',
+#     description: 'Hunter-like green'
+# )
+
+# Colorway.create(
+#     name: 'Blue - Royal',
+#     description: 'Deep rich blue'
+# )
+
+# Colorway.create(
+#     name: 'Blue - Bondi',
+#     description: 'Sky-like blue'
+# )
+
+# Colorway.create(
+#     name: 'Mint',
+#     description: 'greenish blue'
+# )
+
+# Colorway.create(
+#     name: 'Red',
+#     description: 'Just red'
+# )
+
+# Colorway.create(
+#     name: 'Grey',
+#     description: 'Pretty grey'
+# )
+
+# Colorway.create(
+#     name: 'White',
+#     description: 'Plain white'
+# )
+
+# Colorway.create(
+#     name: 'Blue - Tahiti',
+#     description: ''
+# )
+
+# Colorway.create(
+#     name: 'Blue - Cool',
+#     description: ''
+# )
